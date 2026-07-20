f652baf5749848866b6d52ca527dfd5a8ff23605
Voocke <152061996+Voocke@users.noreply.github.com>
2026-07-21 00:17:23 +0300

    test

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -119,7 +119,7 @@ fun EditNumberField(
  * Calculates the tip based on the user input and format the tip amount
  * according to the local currency.
  * Example would be "$10.00".
- */
+ test*/
 private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
     val tip = tipPercent / 100 * amount
     return NumberFormat.getCurrencyInstance().format(tip)
